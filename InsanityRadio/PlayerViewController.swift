b4e759635aa615edc27de564f72e43fc86f2dca8
Dylan Maryk <dylan@dylanmaryk.com>
2015-09-07 14:56:13 +0100

    Use autocorrect parameter.

--- InsanityRadio/PlayerViewController.swift
+++ InsanityRadio/PlayerViewController.swift
@@ -70,7 +70,7 @@ class PlayerViewController: UIViewController {
         nowPlaying = DataModel.getNowPlaying()
         nowPlayingLabel.text = nowPlaying.artist + "\n" + nowPlaying.song
         
-        var url = "http://ws.audioscrobbler.com/2.0/?method=track.getinfo&api_key=38ca8452a5704df8ba7e7de9855844e7&artist=" + nowPlaying.artist + "&track=" + nowPlaying.song + "&format=json"
+        var url = "http://ws.audioscrobbler.com/2.0/?method=track.getinfo&api_key=38ca8452a5704df8ba7e7de9855844e7&artist=" + nowPlaying.artist + "&track=" + nowPlaying.song + "&autocorrect&format=json"
         url = url.stringByReplacingOccurrencesOfString(" ", withString: "%20", options: NSStringCompareOptions.LiteralSearch, range: nil)
         println(url) // Temp
         manager.responseSerializer = AFJSONResponseSerializer()
